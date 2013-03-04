09a3f5640ba16ac4ca1e7b67ad3f2a32698f43f4
Jón Gísli Egilsson <jongisli@Jons-MacBook-Pro.local>
2013-03-04 18:49:00 +0100

    tex and changes

--- regression.py
+++ regression.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import split_data as splitter
 
+img_format = 'png'
+
 def design_matrix(data, columns):
     selection = data.take(columns, axis=1)
     n,_ = selection.shape
@@ -67,24 +69,53 @@ def root_mean_square_error(datafile, design_columns, target_columns, model):
 
     return RMS[0]
 
-def plot_alpha_error(train_file, test_file, design_columns, target_columns):
+def plot_alpha_error(train_file, test_file):
     alpha_range = np.arange(0.001, 0.5, 0.01)
-    errors = []
+    errors1 = []
+    errors2 = []
     for alpha in alpha_range:
-        bayes_model = bayesian_model(train_file, design_columns, target_columns, alpha, 1)
-        errors.append(root_mean_square_error(test_file, design_columns, target_columns, bayes_model))
+        bayes_model1 = bayesian_model(train_file, [3, 6, 7, 8], [1], alpha, 1)
+        bayes_model2 = bayesian_model(train_file, [7], [1], alpha, 1)
+        errors1.append(root_mean_square_error(test_file, [3, 6, 7, 8], [1], bayes_model1))
+        errors2.append(root_mean_square_error(test_file, [7], [1], bayes_model2))
 
-    ml_model = maximum_likelihood_model(train_file, design_columns, target_columns)
-    ml_error = root_mean_square_error(test_file, design_columns, target_columns, ml_model)
+    ml_model1 = maximum_likelihood_model(train_file, [3, 6, 7, 8], [1])
+    ml_model2 = maximum_likelihood_model(train_file, [7], [1])
+    
+    ml_error1 = root_mean_square_error(test_file, [3, 6, 7, 8], [1], ml_model1)
+    ml_error2 = root_mean_square_error(test_file, [7], [1], ml_model2)
 
-    plt.plot(alpha_range, errors)
-    plt.plot(alpha_range, [ml_error]*len(alpha_range))
-    plt.show()
+    
+    plt.plot(alpha_range, errors1, label='$MAP_1$')
+    plt.plot(alpha_range, [ml_error1]*len(alpha_range), label='$ML_1$')
+    plt.legend()
+    plt.savefig('images/rms_selection1.%s' % img_format, format=img_format)
+    plt.close()
+    
+    plt.plot(alpha_range, errors2, label='$MAP_2$')
+    plt.plot(alpha_range, [ml_error2]*len(alpha_range), label='$ML_2$')
+    plt.legend()
+    plt.savefig('images/rms_selection2.%s' % img_format, format=img_format)
     plt.close()
 
     
-if __name__ == "__main__":
-    #splitter.create_training_and_test_sets('data/bodyfatdata.txt', training_ratio = 0.8)
+def average_error(design_columns, target_columns, N):
+    error = 0
+    for i in range(0,N):
+        splitter.create_training_and_test_sets('data/bodyfatdata.txt', training_ratio = 0.8)
+        model = maximum_likelihood_model('data/bodyfat_training.txt',
+                                         design_columns, target_columns)
+        error += root_mean_square_error('data/bodyfat_test.txt',
+                                        design_columns, target_columns, model)
+    return error / float(N)
 
-    plot_alpha_error('data/bodyfat_training.txt', 'data/bodyfat_test.txt', [3, 6, 7, 8], [1])
-    plot_alpha_error('data/bodyfat_training.txt', 'data/bodyfat_test.txt', [7], [1])
+    
+if __name__ == "__main__":
+    #print "The average RMS error for selection 1:"
+    #print average_error([3, 6, 7, 8], [1], 100)
+    #print "The average RMS error for selection 2:"
+    #print average_error([7], [1], 100)
+
+    #splitter.create_training_and_test_sets('data/bodyfatdata.txt', training_ratio = 0.8)
+    print "Plotting MAP vs ML ..."
+    plot_alpha_error('data/bodyfat_training.txt', 'data/bodyfat_test.txt')
